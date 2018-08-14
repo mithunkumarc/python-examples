b366870eda6c8eed179ac2c427d65aef791f5e61
mithunkumarc <mithunchannappa@gmail.com>
2018-08-14 16:19:09 +0530

    Update test.py

--- import all modules/test.py
+++ import all modules/test.py
@@ -1,8 +1,11 @@
 from package1 import *
 print(dir())
 print(Tiger.sound())
+
+
 # before defining __all__ variable
 # ['__annotations__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__']
+
 
 # after defining __all__ variable in __init__.py
 # ['Lion', 'Tiger', '__annotations__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__']
